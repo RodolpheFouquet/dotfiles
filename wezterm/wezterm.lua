c8b7bedf99a8ef0486f8379a7fd2bc95cb5ea1e9
Rodolphe Fouquet <rodolphe@unicowd.com>
2025-05-11 02:21:00 +0100

    add zoxide to wezterm and remove tabline

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 local sessionizer = wezterm.plugin.require("https://github.com/mikkasendke/sessionizer.wezterm")
-
+local zoxide = wezterm.plugin.require("https://github.com/mikkasendke/sessionizer-zoxide.git")
 local my_schema = {
 	sessionizer.DefaultWorkspace({ label_overwrite = "🏠 Home" }),
 	wezterm.home_dir .. "/.config/wezterm",
@@ -9,9 +9,11 @@ local my_schema = {
 	wezterm.home_dir .. "/.config/xmonad",
 	wezterm.home_dir .. "/.config/xmobar",
 	wezterm.home_dir .. "/.config/fish",
+	wezterm.home_dir .. "/.config",
 	wezterm.home_dir .. "/Code",
 	sessionizer.AllActiveWorkspaces({}),
 	sessionizer.FdSearch(wezterm.home_dir .. "/Code"),
+	zoxide.Zoxide({}),
 }
 
 config.keys = {
