fbf6e41dcbf57d088dddc16e8537401238b616cf
Rodolphe Fouquet <rodolphe@unicowd.com>
2026-04-14 22:55:26 +0100

    fix freebsd config for conky

--- i3/.config/i3/ip.sh
+++ i3/.config/i3/ip.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ "$(uname)" = "FreeBSD" ]; then
-    ifconfig wlan0 | grep 'inet ' | awk '{print $2}'
+    ifconfig | grep 'inet ' | awk '{print $2}' | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$' | grep -v '^127\.' | grep -v '^10\.'
 else
     ip -4 addr show wlp11s0 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1
 fi
